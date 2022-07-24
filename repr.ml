751bc7aa912549765aa73fa0bcbcab4dc3dc5b80
amillert <devmood.business@gmail.com>
2022-07-24 19:53:01 +0200

    add smart constructors for data models

--- repr.ml
+++ repr.ml
@@ -2,9 +2,14 @@
 type repr = ClassScope of { id : string; vars : string list } | Empty
 [@@deriving show { with_path = false }]
 
+let classNoVars id = ClassScope { id; vars = [] }
+let classYesVars id vars = ClassScope { id; vars }
+
 (* Semantic representation *)
 type scopeT = { internal : string list; exposed : string list }
 [@@deriving show { with_path = false }]
+
+let emptyScope = { internal = []; exposed = [] }
 
 type classT = { id : string; scope : scopeT }
 [@@deriving show { with_path = false }]
