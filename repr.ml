0ae573558a6e8b41f0d63441b93619ab9aa064b0
amillert <devmood.business@gmail.com>
2022-07-25 12:34:02 +0200

    add extension mode as an additional information stored in AST representation

--- repr.ml
+++ repr.ml
@@ -1,9 +1,14 @@
 (* Abstract Syntax Tree *)
-type repr = ClassScope of { id : string; vars : string list } | Empty
+type extension_mode = Extend | Open [@@deriving show { with_path = false }]
+
+type repr =
+  | ClassScope of { id : string; vars : string list; mode : extension_mode }
+  | Empty
 [@@deriving show { with_path = false }]
 
-let classNoVars id = ClassScope { id; vars = [] }
-let classYesVars id vars = ClassScope { id; vars }
+(* Allow extension by default for more flexibility. *)
+let classNoVars ?(mode = Extend) id = ClassScope { id; vars = []; mode }
+let classYesVars ?(mode = Extend) id vars = ClassScope { id; vars; mode }
 
 (* Semantic representation *)
 type scopeT = { internal : string list; exposed : string list }
@@ -13,3 +18,5 @@ let emptyScope = { internal = []; exposed = [] }
 
 type classT = { id : string; scope : scopeT }
 [@@deriving show { with_path = false }]
+
+(* TODO: decouple into ast and sem modules + smart constructors *)
