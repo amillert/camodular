5f3c1d6a0999369fa6042c98386d38ee82f09a60
amillert <devmood.business@gmail.com>
2022-07-30 21:40:56 +0200

    make example runnable

--- src/repr/ast.ml
+++ src/repr/ast.ml
@@ -9,6 +9,13 @@ type ast =
   | Invalid
 [@@deriving show { with_path = false }]
 
+let show : ast -> string = function
+  | ClassScope { id; vars; _ } ->
+      let part acc x = acc ^ " " ^ x in
+      "ClassScope " ^ id ^ List.fold_left part "" vars
+  | Empty -> "Empty"
+  | Invalid -> "Invalid"
+
 let classNoVars ?(mode = NoInherit) id = ClassScope { id; vars = []; mode }
 let classYesVars ?(mode = NoInherit) id vars = ClassScope { id; vars; mode }
 
