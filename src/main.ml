d2ca64a86b1b9b2715be80d400c514ad440f0f1d
amillert <devmood.business@gmail.com>
2022-08-01 22:51:18 +0200

    graceful exception recovery

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,23 @@
-open Sedlexing
-
 let process filename () =
-  let lexbuf = filename |> open_in |> Utf8.from_channel in
+  let open Repr.Ast in
+  let open Env in
+  let lexbuf = filename |> open_in |> Sedlexing.Utf8.from_channel in
+  let expectFailure =
+    filename |> String.split_on_char '_' |> List.rev |> List.hd = "fail"
+  in
   let parse = MenhirLib.Convert.Simplified.traditional2revised Parser.program in
-  let asts = lexbuf |> Lexer.lex |> parse in
-  List.iter Repr.Ast.print asts
+  let asts =
+    try lexbuf |> Lexer.lex |> parse with
+    | Env.EnvException msg when expectFailure ->
+        let () = print_endline @@ "Graceful recovery from EnvError: " ^ msg in
+        [ Invalid ]
+    | _ -> failwith "Unexpected error; can't recover - BOOOM !"
+  in
+  List.iter print asts
 
 let () =
   let open Util.Syntax in
-  let filenameA = "./files/testa" in
-  let filenameB = "./files/testb" in
-  process filenameA *> process filenameB @> process filenameA
+  let filename1 = "./files/testa_ok" in
+  let filename2 = "./files/testb_ok" in
+  let filename3 = "./files/testc_fail" in
+  process filename1 *> process filename3 @> process filename2
